96b7560f4a31d90c0a813f50382daa24930cce62
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-08-26 10:36:05 +0200

    Add feature test for API documentation

---
+++ features/steps/api_docs.py
@@ -0,0 +1,9 @@
+from behave import *
+
+@when('I open the API documentation')
+def step_impl(context):
+    pass
+
+@then('the API documentation will be returned')
+def step_impl(context):
+    assert True is False
