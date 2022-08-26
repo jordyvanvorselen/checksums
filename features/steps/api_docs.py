e039829b8d993111d65bcd4f75aac4320c265835
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-08-26 10:46:47 +0200

    Add linter and formatter

--- features/steps/api_docs.py
+++ features/steps/api_docs.py
@@ -1,9 +1,11 @@
 from behave import *
 
-@when('I open the API documentation')
+
+@when("I open the API documentation")
 def step_impl(context):
     pass
 
-@then('the API documentation will be returned')
+
+@then("the API documentation will be returned")
 def step_impl(context):
     assert True is False
